b38af9c31a5bc76af1be3ca00a77a842f069f0e4
jzonthemtn <jeff.zemerick@mtnfog.com>
2025-04-03 14:00:37 -0400

    #44 Fixing names.
    Signed-off-by: jzonthemtn <jeff.zemerick@mtnfog.com>

--- src/main/java/org/opensearch/searchrelevance/plugin/rest/SearchRelevanceRestHandler.java
+++ src/main/java/org/opensearch/searchrelevance/plugin/rest/SearchRelevanceRestHandler.java
@@ -578,7 +578,7 @@ public class SearchRelevanceRestHandler extends BaseRestHandler {
                             channel.sendResponse(
                                 new BytesRestResponse(
                                     RestStatus.INTERNAL_SERVER_ERROR,
-                                    "{\"error\": \"Unable to get query sets: " + ex.getMessage() + "\"}"
+                                    "{\"error\": \"Unable to get judgments: " + ex.getMessage() + "\"}"
                                 )
                             );
                         }
